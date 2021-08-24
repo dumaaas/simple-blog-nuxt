dca9aed6d84978df4b8a514aab590b8e1a42594c
Marko Dumnic <markodumnic8@gmail.com>
2021-08-24 17:06:43 +0200

    Adding all

---
+++ pages/about.vue
@@ -0,0 +1,77 @@
+<template>
+  <section>
+    <div class="about">
+      <div class="about-title">
+        <div class="about-title__img">
+          <img src="@/assets/images/me.jpg" alt="logo" />
+        </div>
+        <div class="about-title__heading">
+          <h2>About Me</h2>
+        </div>
+      </div>
+      <div class="about-content">
+        <p>
+          Hey, I'm Marko! I made this template. If you're reading this, you
+          should probably be changing the content on this page. You can do that
+          in <code>pages/about/index.vue.
+              </code>
+        </p>
+        <p>
+          Want to change the image above? (Hopefully!) Add your own in <code>
+assets/images
+          </code>
+          . You can replace mine and title yours <code>me.jpg</code> (or, rename
+          it and change the file path above).
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+section {
+  height: 100vh;
+  background-color: #1e1e1e;
+}
+.about {
+  margin: 0 auto;
+  padding: 180px 20px 0 20px;
+  max-width: 78ch !important;
+  &-title {
+    display: flex;
+    gap: 15px;
+    align-items: center;
+    &__img {
+      img {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+      }
+    }
+    &__heading {
+      h2 {
+        font-size: 56px;
+        line-height: 84px;
+        color: #f5f5f5;
+        font-weight: 800;
+      }
+    }
+  }
+  &-content {
+      p {
+          font-size: 19px;
+          color: #f5f5f5;
+          font-weight: 400;
+          line-height: 29px;
+          padding-top: 50px;
+          &:nth-child(2) {
+              padding-top: 20px;
+          }
+      }
+  }
+}
+</style>
---
+++ pages/blog.vue
@@ -0,0 +1,134 @@
+<template>
+  <section>
+    <div class="blog">
+      <div class="blog-title">
+        <h2>Blog Posts</h2>
+      </div>
+      <div class="blog-content">
+        <nuxt-link
+          v-for="blog in blogs"
+          :key="blog.title"
+          :to="`/items/${blog.id}`"
+          ><div class="blog-content__item">
+            <div class="blog-content__item-top">
+              <img :src="require(`@/assets/images/${blog.img}`)" alt="" />
+            </div>
+            <div class="blog-content__item-bottom">
+              <h3>{{ blog.title }}</h3>
+              <span>{{ blog.date }}</span>
+              <p>{{ blog.content }}</p>
+            </div>
+          </div></nuxt-link
+        >
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: [
+        {
+          id: 1,
+          title: "How to Blog",
+          date: "MARCH 2021",
+          content: "Gettin started with the Nuxt Blog Starter template.",
+          img: "howToBlog.jpg",
+        },
+        {
+          id: 2,
+          title: "Dogs",
+          date: "JANUARY 2021",
+          content: "Dogs are the best animal. Here's why.",
+          img: "howToBlog.jpg",
+        },
+        {
+          id: 3,
+          title: "Cats",
+          date: "DECEMBER 2020",
+          content: "Cats are the second best animal. Here's why.",
+          img: "howToBlog.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+  height: 100vh;
+  background-color: #1e1e1e;
+}
+.blog {
+  margin: 0 auto;
+  padding: 180px 20px 0 20px;
+  max-width: 78ch !important;
+  &-title {
+    display: flex;
+    gap: 15px;
+    align-items: center;
+
+    h2 {
+      font-size: 56px;
+      line-height: 84px;
+      color: #f5f5f5;
+      font-weight: 800;
+    }
+  }
+  &-content {
+    display: flex;
+    padding-top: 50px;
+    gap: 20px;
+    a {
+      width: 33.33%;
+    }
+    &__item {
+      align-self: stretch;
+      width: 100%;
+      border-radius: 5px;
+      background-color: #0f0f0f;
+      height: 100%;
+      transform: translateX(0px) translateY(0px);
+      transition: transform 1s ease 0s;
+
+      img {
+        width: 100%;
+        border-top-left-radius: 5px;
+        border-top-right-radius: 5px;
+        object-fit: cover;
+        height: 97px;
+      }
+
+      &-bottom {
+        padding: 22px;
+        h3 {
+          font-size: 24px;
+          line-height: 27px;
+          font-weight: 600;
+          padding-bottom: 5px;
+        }
+        span {
+          font-size: 16px;
+          line-height: 20px;
+          color: #f5f5f5;
+          font-weight: 300;
+        }
+        p {
+          padding-top: 20px;
+          font-size: 16px;
+          line-height: 24px;
+          color: #f5f5f5;
+          font-weight: 400;
+        }
+      }
+      &:hover {
+        transition: transform 1s ease 0s;
+        transform: translateX(0px) translateY(-10px);
+      }
+    }
+  }
+}
+</style>
